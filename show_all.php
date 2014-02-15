9a25de7fb239e0117b9c4fd7a3af1332caabb63f
heldersepu <heldersepu@5cf47647-7d64-d51c-be14-62403fbcf44f>
2014-02-15 00:02:09 +0000

    add try catch

--- show_all.php
+++ show_all.php
@@ -11,15 +11,19 @@ include_once 'db.php';
 function action()
 {
 	global $db;
-	$query = 'SELECT * FROM `v_store` ';
-    $mysqli = new mysqli($db->server, $db->user, $db->pasw, $db->defaultdb);
-    $result = $mysqli->query($query);
     $data = array();
-    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-        $data[] = $row;
-    }
-    $result->close();
-    $mysqli->close();
+	try {
+		$query = 'SELECT * FROM `v_store` ';
+		$mysqli = new mysqli($db->server, $db->user, $db->pasw, $db->defaultdb);
+		$result = $mysqli->query($query);
+		while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+			$data[] = $row;
+		}
+		$result->close();
+		$mysqli->close();
+	} catch (Exception $e) {
+		$data['error'] = $e->getMessage();
+	}
 	return json_encode($data);
 }
 
