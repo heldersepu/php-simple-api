84322440eba7c3a875a7ddf1fc0870f2229f6942
heldersepu <heldersepu@5cf47647-7d64-d51c-be14-62403fbcf44f>
2014-04-14 23:52:19 +0000

    new method

---
+++ trunk/echo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file   echo.php
+ * @brief  Shows the same items given in the post
+ *
+ * Sample call: <a href="/api/echo.php">/api/echo.php</a>
+ */
+
+
+function action()
+{
+    $data = array();
+	$data["count"] = count($_POST);
+	foreach ($_POST as $k => $v) {
+		$data[$k] = $v;
+	}
+	return json_encode($data);
+}
+
+echo action();
+?>
