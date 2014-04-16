1556124472584bc682fcd66369985451e70f2dc4
heldersepu <heldersepu@5cf47647-7d64-d51c-be14-62403fbcf44f>
2014-04-16 18:19:27 +0000

    more items in the views

--- trunk/views.sql
+++ trunk/views.sql
@@ -9,16 +9,35 @@ SELECT store_id, value as phone
 FROM   `oc_setting`
 WHERE `key`='config_telephone'
 
+CREATE VIEW v_store_fax AS 
+SELECT store_id, value as fax
+FROM   `oc_setting`
+WHERE `key`='config_fax'
+
+DROP VIEW v_store_logo;
+CREATE VIEW v_store_logo AS 
+SELECT store_id, value as logo
+FROM   `oc_setting`
+WHERE `key`='config_logo'
+
+DROP VIEW v_store_address;
 CREATE VIEW v_store_address AS 
 SELECT store_id, value as address
 FROM   `oc_setting`
 WHERE `key`='config_address'
 
+
+
+DROP VIEW v_store;
 CREATE VIEW v_store AS 
-SELECT st.store_id, name, url, email, phone, address
+SELECT st.store_id, name, url, email, phone, fax, logo, address
 FROM  `oc_store` st
 JOIN  `v_store_email` v1 
 	ON st.store_id = v1.store_id
+JOIN  `v_store_logo` vl2 
+	ON st.store_id = vl2.store_id
+JOIN  `v_store_fax` vf2 
+	ON st.store_id = vf2.store_id
 JOIN  `v_store_phone` v2 
 	ON st.store_id = v2.store_id
 JOIN  `v_store_address` v3
